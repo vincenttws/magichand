f1ad46d7cb351cfc293fdd5f1968909895bd571b
Vincent Tan <vincent.tan@shopee.com>
2023-04-04 07:52:30 +0800

    Update css

--- src/Landing.js
+++ src/Landing.js
@@ -4,27 +4,29 @@ import './Landing.css';
 function Landing() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="header-buttons">
-          <button className="Login-button">Login</button>
-          <button className="Sign-up-button">Sign Up</button>
-        </div>
-        <div className="header-description">
-          <div className="header-title">
-            <h1 className="header-title-one">Intelligent and Intuitive</h1>
-            <h1 className="header-title-two">Stock Portfolio Management</h1>
+      <div className="top-middle">
+        <header className="App-header inter-font">
+          <div className="top-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="header-buttons">
+              <button className="Login-button">Login</button>
+              <button className="Sign-up-button">Sign Up</button>
+            </div>
           </div>
-          <p>Only the best can serve you right and we have confident to prove it to you</p>
-        </div>
-        
-      </header>
-      <body>
-
-      </body>
+          <div className="header-description">
+            <div className="header-title">
+              <h1 className="header-title-one">Intelligent and Intuitive<br></br>Stock Portfolio Management</h1>
+            </div>
+            <p className="header-text">Only the best can serve you right and we have confident to prove it to you</p>
+          </div>
+        </header>
+        <body>
+        </body>
+      </div>
+      
       <footer>
         <div className="footer-div">
-          <div className="footer-text">
+          <div className="footer-text inter-font">
             <h1>MAGICHAND</h1>
             <p>Made with ❤️. All rights reserved</p>
           </div>
