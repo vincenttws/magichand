1189f132257b38de615adc26cfa895334776f25c
Vincent Tan <vincent.tan@shopee.com>
2023-04-04 12:14:01 +0800

    Added features section

--- src/Landing.js
+++ src/Landing.js
@@ -1,5 +1,39 @@
 import logo from './logo.svg';
+import intelligentLogo from './intelligent_logo.svg';
+import intuitiveLogo from './intuitive_logo.svg';
+import moreLogo from './more_logo.svg';
 import './Landing.css';
+import { useState } from 'react';
+
+const features = [
+  {
+    title: 'Intelligent',
+    description: 'Meet our automatically generated intelligent report everyday you come visit. All you need to do is browse.',
+    featurelogo: intelligentLogo
+  },
+  {
+    title: 'Intuitive',
+    description: 'Get to know your portfolio as you have seen it before. You will come and will never go, we can be sure of it.',
+    featurelogo: intuitiveLogo
+  },
+  {
+    title: 'Alot more...',
+    description: 'More ways we can flash ourself. You wouldn’t be disapointed by what we are offering.',
+    featurelogo: moreLogo
+  }
+];
+
+function FeaturesBox({ feature }) {
+  return (
+      <div className="inter-font feature-box">
+        <div style={{ paddingTop: "20px" }}>
+          <h3 style={{ display: "inline-block", fontWeight: "bold", fontSize: "20px" }}>{feature.title}</h3>
+          <img src={feature.featurelogo} alt="logo" style={{ display: "inline-block", float: "right" }} />
+        </div>
+        <p>{feature.description}</p>
+      </div>
+  );
+}
 
 function Landing() {
   return (
@@ -21,13 +55,18 @@ function Landing() {
           </div>
         </header>
         <body>
+          <div className="feature-section" style={{ display: "inline-block" }}>
+            <FeaturesBox feature={features[0]} style={{ display: "inline-block" }} />
+            <FeaturesBox feature={features[1]} style={{ display: "inline-block" }} />
+            <FeaturesBox feature={features[2]} style={{ display: "inline-block" }} />
+          </div>
         </body>
       </div>
       
       <footer>
         <div className="footer-div">
           <div className="footer-text inter-font">
-            <h1>MAGICHAND</h1>
+            <h1 style={{ fontSize: "50px", marginBottom: "20px" }}>MAGICHAND</h1>
             <p>Made with ❤️. All rights reserved</p>
           </div>
         </div>
